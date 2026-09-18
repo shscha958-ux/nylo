c3f4cfb8cbc5cac4ff6c8548feec7a2df1000a3f
shscha958-ux <shscha958@gmail.com>
2026-09-18 10:08:40 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -201,4 +201,4 @@ async def clear_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
         await ctx.send("عذراً، لا تمتلك صلاحية مسح الرسائل.", delete_after=5)
 
-bot.run("MTU1MDAwMzIzMDM4NDE5MzU5Ng.Gmwu-a.Mrh_k1SZhtFJUe_JdO2QCSG-5rIPNyv2hehu-4")
+bot.run("MTU1MDAwMzIzMDM4NDE5MzU5Ng.GVVEMq.dIkxnGW_sP_FUKk-xMNk6ARZDQJyqWVbejkh3Y")
